3cf1bd856a5503f3f0338da4764045a982cee65e
Nikita <ns45134513@gmail.com>
2026-05-25 17:45:34 +0530

    second

--- student.py
+++ student.py
@@ -30,12 +30,12 @@ X = s.fit_transform(X)
 
 X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.2,random_state=42)
 
-log = LogisticRegression(max_iter=10000)
-m= log.fit(X_train,y_train)
+l = LogisticRegression(max_iter=10000)
+m= l.fit(X_train,y_train)
 n = m.predict(X_test)
 print(n)
 
-print("Accuracy Score : ",accuracy_score(y_test,n))
+print("Accuracy Score Logistic Regression: ",accuracy_score(y_test,n))
 
 # o = SVC()
 # p = o.fit(X_train,y_train)
@@ -46,4 +46,34 @@ k = KNeighborsClassifier(n_neighbors=5)
 
 r = k.fit(X_train,y_train)
 s = r.predict(X_test)
-print("Accuracy Score : ",accuracy_score(y_test,s))
+print("Accuracy Score KNN : ",accuracy_score(y_test,s))
+
+b = DecisionTreeClassifier()
+
+c = b.fit(X_train,y_train)
+d = c.predict(X_test)
+print("Accuracy Score Decision Tree : ",accuracy_score(y_test,d))
+
+e = RandomForestClassifier()
+
+f = e.fit(X_train,y_train)
+g = f.predict(X_test)
+print("Accuracy Score Random Forest: ",accuracy_score(y_test,g))
+
+h = GaussianNB()
+# i = MultinomialNB()
+j = BernoulliNB()
+
+t = h.fit(X_train,y_train)
+# u = i.fit(X_train,y_train)
+v = j.fit(X_train,y_train)
+
+w = t.predict(X_test)
+# x = u.predict(X_test)
+y = v.predict(X_test)
+
+print("Accuracy Score GaussianNB : ",accuracy_score(y_test,w))
+# print("Accuracy Score MultinomialNB: ",accuracy_score(y_test,x))
+print("Accuracy Score BernoulliNB : ",accuracy_score(y_test,y))
+
+
